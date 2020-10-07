0888a033b8d5fc642b923320ea405a0398fc2a0c
Aman <amn.gusi@gmail.com>
2020-10-07 19:59:48 +0530

    Updates

---
+++ precache-manifest.ef26a224990e69ef394ae29ed144abff.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fdfb070fc9ea7e9d837327b9a342da",
+    "url": "/Agence/index.html"
+  },
+  {
+    "revision": "3834bf43105ffb88f1e8",
+    "url": "/Agence/static/css/2.9fd47868.chunk.css"
+  },
+  {
+    "revision": "79746074fb6bbb877fc5",
+    "url": "/Agence/static/css/main.abbb9f26.chunk.css"
+  },
+  {
+    "revision": "3834bf43105ffb88f1e8",
+    "url": "/Agence/static/js/2.49b37c81.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/Agence/static/js/2.49b37c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79746074fb6bbb877fc5",
+    "url": "/Agence/static/js/main.389f455b.chunk.js"
+  },
+  {
+    "revision": "929571137253e99a22f9",
+    "url": "/Agence/static/js/runtime-main.4ed8951e.js"
+  },
+  {
+    "revision": "ba833ac476d62cd9e008d924ec9ff5f0",
+    "url": "/Agence/static/media/brand.ba833ac4.jpg"
+  },
+  {
+    "revision": "596e15871dee23310c229715623a83dd",
+    "url": "/Agence/static/media/design.596e1587.jpg"
+  },
+  {
+    "revision": "44739226b36c273290f487c25d0b0f4c",
+    "url": "/Agence/static/media/development.44739226.jpg"
+  },
+  {
+    "revision": "8301fc9d0e4d4662c4a8694e60125cf8",
+    "url": "/Agence/static/media/identity.8301fc9d.jpg"
+  },
+  {
+    "revision": "669abd69117d5b91f55ca79be9b5e470",
+    "url": "/Agence/static/media/production.669abd69.jpg"
+  },
+  {
+    "revision": "97f19feb62e1a8fb9d96c2c6132cf391",
+    "url": "/Agence/static/media/research.97f19feb.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Agence/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Agence/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Agence/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Agence/static/media/slick.f97e3bbf.svg"
+  }
+]);
